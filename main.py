3dc3202d2aa0b5a6748beaef6ef1203fb5bf30b1
homepirate <el2409@yandex.ru>
2024-03-02 00:49:51 +0300

    add fun for save data and change method in bst

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 from haversine import haversine
 import pandas as pd
 
@@ -25,11 +27,37 @@ class MainPoint(Point):
         self.bst = BST(self.coords())
 
     def make_bst(self, points: list[Point]):
+        """Сторит BST для точки"""
         for point in points:
             self.bst.add(Node(data=point))
 
     def get_nears(self, limit):
-        return self.bst.postorder(limit)
+        """Находит ближайшие точки с указанными лимитом """
+        return self.bst.inorder(limit)
+
+    def make_json(self, limit):
+        """Метод записывает данные в json формате для каждой точки и ее ближайших"""
+        near_points = self.get_nears(limit)
+        points_list = []
+
+        for point in near_points:
+            point_data = {
+                'name': point.name,
+                'coords': point.coords(),
+                'dist':  haversine(self.coords(), point.coords())
+            }
+            points_list.append(point_data)
+
+        json_data = {
+            'main_point': {
+                'name': self.name,
+                'coords': self.coords()
+            },
+            'near_points': points_list
+        }
+
+        json_string = json.dumps(json_data, indent=4, ensure_ascii=False)
+        return json_string
 
 
 class Node:
@@ -69,52 +97,87 @@ class BST:
                         current.right_child = new_item
                         break
 
-    def postorder(self, limit):
-        """Метод обхода  postorder сначала проверяем самого левого потомка (он самый маленький) потом проверяем центрального, потом правого
+    def inorder(self, limit):
+        """Метод обхода inorder сначала проверяем самого левого потомка (он самый маленький) потом проверяем центрального, потом правого
         Так происхожит сортировка по возрастанию в BST"""
 
         stack = []
         result = []
         current = self.root
-        last_visited = None
 
         while stack or current:
             if current:
                 stack.append(current)
                 current = current.left_child
             else:
-                peek_node = stack[-1]
-                if peek_node.right_child and last_visited != peek_node.right_child:
-                    current = peek_node.right_child
-                else:
-                    node = stack.pop()
-                    result.append(node.data)
-                    limit -= 1
-                    last_visited = node
-            if limit == 0:
-                break
+                node = stack.pop()
+                result.append(node.data)
+                limit -= 1
+                if limit == 0:
+                    break
+                current = node.right_child
 
         return result
 
 
 def open_files(file_path):
+    """Открывает файл в форматах csv xls xlsx"""
     if file_path.endswith('.csv'):
         data = pd.read_csv(file_path, delimiter=';', header=None)
     elif file_path.endswith('.xls') or file_path.endswith('.xlsx'):
         data = pd.read_excel(file_path, header=None)
     else:
         print("Unsupported file format. Please provide a CSV, XLS, or XLSX file.")
-        return []
+        return
+
+    return data
+
+
+def make_arrays(file_a, file_b) -> tuple[list[MainPoint], list[Point]]:
+    """Функция которая создает и возвращает два массива"""
+    data_a = open_files(file_a)
+    data_b = open_files(file_b)
+    main_points = []
+    for index, row in data_a.iterrows():
+        new_point = MainPoint(row.iloc[0], row.iloc[1])
+        main_points.append(new_point)
 
     points = []
-    for index, row in data.iterrows():
+    for index, row in data_b.iterrows():
         new_point = Point(row.iloc[0], row.iloc[1])
         points.append(new_point)
 
-    return points
+    return main_points, points
+
+
+def create_xlsx_from_json(result_json_array, file_path):
+    """Функция для сохранения итоговых данных"""
+    all_data = []
+
+    for js in result_json_array:
+        json_data = json.loads(js)
+        main_point_name = json_data['main_point']['name']
+        main_point_coords = ', '.join(map(str, json_data['main_point']['coords'])) # Координаты преобразуем в строку в формате XX.xxx, XX.xxx
+
+        near_points_data = []
+        for near_point in json_data['near_points']:
+            near_point_name = near_point['name']
+            near_point_coords = ', '.join(map(str, near_point['coords']))
+            near_point_dist = near_point['dist']
+            near_points_data.extend([near_point_coords, near_point_name, near_point_dist]) # Добавляем в итоговый массив с данными всех ближайших точек для конкретной точки
+
+        all_data.append([main_point_name, main_point_coords] + near_points_data)
+
+    columns = ['Main Point Name', 'Main Point Coords']
+    for i in range(len(json_data['near_points'])):
+        columns += [f"Near Point Coords_{i+1}", f"Near Point Name_{i+1}", f"Distance_{i+1}"] # Создаем дополнительные колонки для каждой из ближайших точек они имеют индексы
+
+    df = pd.DataFrame(all_data, columns=columns)
+    df.to_excel(file_path, index=False)
 
 
 def main():
+    LIMIT = 3
     # mn = MainPoint("fff", "1.2, 3.4")
     # points = [Point("point1", "34.1, 14.2"), Point("point2", "1.1, 3.4"), Point("point3", "1.2, 3.3"),
     #           Point("point4", "44.1, 44.2"),]
@@ -124,8 +187,17 @@ def main():
     # print(haversine((1.2, 3.4), (1.1, 3.4)))
     # print(haversine((1.2, 3.4), (1.2, 3.3)))
     # print(haversine((55.613305, 37.604573), (55.612314, 37.589143)))
-    print(open_files("test_data/A.csv"))
-    print(open_files("test_data/A.xlsx"))
+
+    result_json_array = []
+    main_points, points = make_arrays("test_data/A.xlsx", "test_data/B.xlsx")
+    for mp in main_points:
+        mp.make_bst(points)
+
+        result_json_array.append(mp.make_json(LIMIT))
+
+    create_xlsx_from_json(result_json_array, "test_data/res.xlsx")
+
+    # print(make_arrays("test_data/A.xlsx", "test_data/B.xlsx"))
 
 
 if __name__ == '__main__':
